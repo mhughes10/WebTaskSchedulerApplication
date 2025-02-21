6f24137ad4d6f69240c5f9de12e718d83d7e05bd
Marshall Hughes <bathavenue@hotmail.com>
2025-02-21 11:43:19 -0500

    test

--- AlprImageModeWebApp/MainApp/Forms.py
+++ AlprImageModeWebApp/MainApp/Forms.py
@@ -3,17 +3,18 @@ from django import forms
 from django.core.exceptions import ValidationError
 
 def validateIp(ip): #          or          or       ("?" allows 2-9 or 10-99)
-                       #250-255  200-249     100-199       10-99    2-9   0-1 (.) repeat...
+                   #250-255  200-249     100-199       10-99    2-9   0-1 (.) repeat...
     ipPattern = r'^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[2-9]|[01])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[2-9]|[01])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[2-9]|[01])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[2-9]|[01])$'
-    domainPattern = "^((?!-)[A-Za-z0-9-]{1, 63}(?<!-)\\.)+[A-Za-z]{2, 6}$"
-                                                                                           
-    raise ValidationError("Please enter Ip in the required format (25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[2-9]|[01])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[2-9]|[01])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[2-9]|[01])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[2-9]|[01])")
+    domainPattern = r'^(?:[a-zA-Z0-9-]{1,63}\.)+[a-zA-Z]{2,6}$'
+    
+    if not (re.match(ipPattern, ip) or re.match(domainPattern, ip)):                                                                                    
+        raise ValidationError("Please enter Ip in the required format (25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[2-9]|[01])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[2-9]|[01])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[2-9]|[01])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[2-9]|[01])")
     
 def validateParseCorrectFormat(credential):
     
     if credential.find(":") > -1:
         raise ValidationError('":" is an Invalid character')
-
+    
 class SetTwoSchedulerTimes(forms.Form):
     startTime1 = forms.TimeField(label="Enable Time", input_formats=['%H:%M'], error_messages={'invalid': 'Invalid format'}, widget=forms.TimeInput(attrs={'type': 'time'}))
     startTime2 = forms.TimeField(label="Disable Time", input_formats=['%H:%M'], error_messages={'invalid': 'Invalid format'}, widget=forms.TimeInput(attrs={'type': 'time'}))
@@ -23,7 +24,7 @@ class SetOneSchedulerTime(forms.Form):
     
 class IsEnabled(forms.Form):
     isEnabled = forms.BooleanField(label="Scheduler Enabled", required = False)
-
+    
 class CamDetails(forms.Form):
     endUrl = forms.CharField(validators=[validateIp], label="IP", required = True)
     endPort = forms.IntegerField(label="Port", max_value=65535, min_value=0, required = True)
